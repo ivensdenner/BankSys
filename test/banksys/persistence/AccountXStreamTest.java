2f6b036754da85ed63e733238d4daf2b9fbe3f45
ivensdenner <ivensdenner@gmail.com>
2016-10-07 09:39:30 -0300

    Refactor AccountXStreamTest

--- test/banksys/persistence/AccountXStreamTest.java
+++ test/banksys/persistence/AccountXStreamTest.java
@@ -1,13 +1,15 @@
 package banksys.persistence;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import banksys.account.AbstractAccount;
-import banksys.account.OrdinaryAccount;
+import banksys.account.exception.InsufficientFundsException;
+import banksys.account.exception.NegativeAmountException;
 import banksys.persistence.exception.AccountCreationException;
 import banksys.persistence.exception.AccountDeletionException;
 import banksys.persistence.exception.AccountNotFoundException;
@@ -15,49 +17,44 @@ import banksys.persistence.exception.FlushException;
 
 public class AccountXStreamTest {
 	
+	private static final String ACCOUNT_NUMBER_A = "1234";
+	private static final String ACCOUNT_NUMBER_B = "5678";
 	private AccountXStream repository;
-	private AbstractAccount account;
+	private AbstractAccount accountA;
+	private AbstractAccount accountB;
 
 	@Before
 	public void setUp() throws Exception {
 		repository = new AccountXStream();
-		account = new OrdinaryAccount("1234");
+		accountA = accountMock(true);
+		repository.create(accountA);
+		accountB = accountMock(false);
+	}
+	
+	private AbstractAccount accountMock(boolean isA) {
+		String accountNumber = isA ? ACCOUNT_NUMBER_A : ACCOUNT_NUMBER_B;
+		return new AbstractAccount(accountNumber) {
+			@Override
+			public void debit(double amount) throws NegativeAmountException, InsufficientFundsException {
+			}
+		};
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		try {
-			repository.delete(account.getNumber());
+			repository.delete(ACCOUNT_NUMBER_A);
+			repository.delete(ACCOUNT_NUMBER_B);
+		} catch (AccountDeletionException e) {
+		} finally {
 			repository.flush();
-		} catch (AccountDeletionException | FlushException e) {
 		}
-	}
-
-	@Test
-	public void testCreate() {
-		try {
-			repository.create(account);
-			repository.flush();
-		} catch (AccountCreationException | FlushException e) {
-			fail(e.getMessage());
-		}
-		
-		repository = new AccountXStream();
-		
-		assertTrue("Number of accounts should be 1", repository.mumberOfAccounts() == 1);
 	}
 	
 	@Test
 	public void testDelete() {
 		try {
-			repository.create(account);
-			repository.flush();
-		} catch (AccountCreationException | FlushException e) {
-			fail(e.getMessage());
-		}
-		
-		try {
-			repository.delete(account.getNumber());
+			repository.delete(ACCOUNT_NUMBER_A);
 			repository.flush();
 		} catch (AccountDeletionException | FlushException e) {
 			fail(e.getMessage());
@@ -69,9 +66,20 @@ public class AccountXStreamTest {
 	}
 	
 	@Test
-	public void testRetrieve() {
+	public void testDeleteAccountDeletionException() {
 		try {
-			repository.create(account);
+			repository.delete("0000");
+		} catch (AccountDeletionException e) {
+			return;
+		}
+		
+		fail("Should throw AccountDeletionException");
+	}
+	
+	@Test
+	public void testCreate() {
+		try {
+			repository.create(accountB);
 			repository.flush();
 		} catch (AccountCreationException | FlushException e) {
 			fail(e.getMessage());
@@ -79,28 +87,46 @@ public class AccountXStreamTest {
 		
 		repository = new AccountXStream();
 		
-		AbstractAccount retrievedAccount = null;
+		assertTrue("Number of accounts should be 2", repository.mumberOfAccounts() == 2);
+	}
+	
+	@Test
+	public void testCreateAccountCreationException() {
 		try {
-			retrievedAccount = repository.retrieve(account.getNumber());
-		} catch (AccountNotFoundException e) {
-			fail(e.getMessage());
+			repository.create(accountA);
+		} catch (AccountCreationException e) {
+			return;
 		}
 		
-		assertTrue("Account numbers should be equal", account.getNumber().equals(retrievedAccount.getNumber()));
+		fail("Should throw AccountCreationException");
 	}
 	
 	@Test
 	public void testNumberOfAccounts() {
+		assertTrue("Number of accounts should be 1", repository.mumberOfAccounts() == 1);
+	}
+	
+	@Test
+	public void testRetrieve() {
+		AbstractAccount retrievedAccount = null;
 		try {
-			repository.create(account);
-			repository.flush();
-		} catch (AccountCreationException | FlushException e) {
+			retrievedAccount = repository.retrieve(ACCOUNT_NUMBER_A);
+		} catch (AccountNotFoundException e) {
 			fail(e.getMessage());
 		}
-		
-		repository = new AccountXStream();
-		
-		assertTrue("Number of accounts should be 1", repository.mumberOfAccounts() == 1);
+
+		assertTrue("Returned wrong account", ACCOUNT_NUMBER_A == retrievedAccount.getNumber());
+	}
+	
+	@Test
+	public void testRetrieveAccountNotFoundException() {
+		try {
+			repository.retrieve("0000");
+		} catch (AccountNotFoundException e) {
+			return;
+		}
+
+		fail("Should throw AccountNotFoundException");
 	}
 
 }
