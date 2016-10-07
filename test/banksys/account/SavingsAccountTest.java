35481d4116593c11164f499ab395601b2d8d66ee
ivensdenner <ivensdenner@gmail.com>
2016-10-06 22:34:51 -0300

    Refactor SavingAccountTest

--- test/banksys/account/SavingsAccountTest.java
+++ test/banksys/account/SavingsAccountTest.java
@@ -1,44 +1,26 @@
 package banksys.account;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-import banksys.account.exception.NegativeAmountException;
-
 public class SavingsAccountTest {
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
+	
+	private static final String ACCOUNT_NUMBER = "1234";
+	private SavingsAccount account;
 
 	@Before
 	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
+		account = new SavingsAccount(ACCOUNT_NUMBER);
+		account.credit(50);
 	}
 
 	@Test
 	public void testEarnInterest() {
-		SavingsAccount account = new SavingsAccount("1234");
-		try {
-			account.credit(50);
-		} catch (NegativeAmountException e) {
-			fail(e.getMessage());
-		}
-		
 		account.earnInterest();
-		assertTrue("Balance should be " + (50 + (50 * 0.001)) + ", is " + account.getBalance(), account.getBalance() == 50 + (50 * 0.001));
+		double expectedBalance = 50 + (50 * 0.001);
+		assertTrue("Balance should be " + expectedBalance, account.getBalance() == expectedBalance);
 	}
 
 }
