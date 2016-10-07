ef9107bf1ef8c4f918ff94eda00806275bccd216
ivensdenner <ivensdenner@gmail.com>
2016-10-07 11:14:37 -0300

    Refactor and add a lot ofn new tests to BankControllerTest

--- test/banksys/control/BankControllerTest.java
+++ test/banksys/control/BankControllerTest.java
@@ -1,18 +1,18 @@
 package banksys.control;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import banksys.account.AbstractAccount;
-import banksys.account.OrdinaryAccount;
+import banksys.account.SavingsAccount;
+import banksys.account.SpecialAccount;
 import banksys.account.exception.InsufficientFundsException;
 import banksys.account.exception.NegativeAmountException;
 import banksys.control.exception.BankTransactionException;
+import banksys.control.exception.IncompatibleAccountException;
 import banksys.persistence.IAccountRepository;
 import banksys.persistence.exception.AccountCreationException;
 import banksys.persistence.exception.AccountDeletionException;
@@ -21,25 +21,36 @@ import banksys.persistence.exception.FlushException;
 
 public class BankControllerTest {
 	
+	private static final String INVALID_ACCOUNT_NUMBER = "0000";
+	private static final String ACCOUNT_NUMBER_A = "1234";
+	private static final String ACCOUNT_NUMBER_B = "5678";
+	private static final String SAVINGS_ACCOUNT_NUMBER = "9999";
+	private static final String SPECIAL_ACCOUNT_NUMBER = "1010";
+	
 	private IAccountRepository repository;
-	private AbstractAccount account;
+	
+	private AbstractAccount accountA;
+	private AbstractAccount accountB;
+	private SavingsAccount savingsAccount;
+	private SpecialAccount specialAccount;
+	
 	private BankController controller;
+	
 	private boolean didCreateAccount;
 	private boolean didRemoveAccount;
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
 
 	@Before
 	public void setUp() throws Exception {
 		repository = newRepositoryMock();
-		account = newAccountMock();
-		account.credit(50);
+		
+		accountA = newAccountMock(true);
+		accountA.credit(50);
+		accountB = newAccountMock(false);
+		savingsAccount = new SavingsAccount(SAVINGS_ACCOUNT_NUMBER);
+		savingsAccount.credit(50);
+		specialAccount = new SpecialAccount(SPECIAL_ACCOUNT_NUMBER);
+		specialAccount.credit(50);
+		
 		controller = new BankController(repository);
 		
 		didCreateAccount = false;
@@ -48,44 +59,57 @@ public class BankControllerTest {
 	
 	private IAccountRepository newRepositoryMock() {
 		return new IAccountRepository() {
-			
 			@Override
 			public AbstractAccount retrieve(String number) throws AccountNotFoundException {
-				return account;
+				switch (number) {
+				case ACCOUNT_NUMBER_A:
+					return accountA;
+				case ACCOUNT_NUMBER_B:
+					return accountB;
+				case SAVINGS_ACCOUNT_NUMBER:
+					return savingsAccount;
+				case SPECIAL_ACCOUNT_NUMBER:
+					return specialAccount;
+				default:
+					throw new AccountNotFoundException("Invalid account number", number);
+				}
 			}
 			
 			@Override
 			public int mumberOfAccounts() {
-				// TODO Auto-generated method stub
-				return 0;
+				return 4;
 			}
 			
 			@Override
 			public AbstractAccount[] list() {
-				// TODO Auto-generated method stub
-				return null;
+				return new AbstractAccount[]{accountA, accountB, savingsAccount};
 			}
 			
 			@Override
 			public void flush() throws FlushException {
-				// TODO Auto-generated method stub
 			}
 			
 			@Override
 			public void delete(String number) throws AccountDeletionException {
+				if (number != ACCOUNT_NUMBER_A) {
+					throw new AccountDeletionException("Invalid account number", number);
+				}
 				didRemoveAccount = true;
 			}
 			
 			@Override
 			public void create(AbstractAccount account) throws AccountCreationException {
+				if (account.getNumber() != ACCOUNT_NUMBER_A) {
+					throw new AccountCreationException("Invalid account", account.getNumber());
+				}
 				didCreateAccount = true;
 			}
 		};
 	}
 	
-	private AbstractAccount newAccountMock() {
-		return new AbstractAccount("1234") {
-			
+	private AbstractAccount newAccountMock(boolean isA) {
+		String accountNumber = isA ? ACCOUNT_NUMBER_A : ACCOUNT_NUMBER_B;
+		return new AbstractAccount(accountNumber) {
 			@Override
 			public void debit(double amount) throws NegativeAmountException, InsufficientFundsException {
 				if (amount < 0) {
@@ -100,58 +124,275 @@ public class BankControllerTest {
 		};
 	}
 
-	@After
-	public void tearDown() throws Exception {
-	}
-
 	@Test
 	public void testAddAccount() {
 		try {
-			controller.addAccount(account);
+			controller.addAccount(accountA);
 		} catch (BankTransactionException e) {
 			fail(e.getMessage());
 		}
 		
-		assertTrue("Account wasn't created in the repository", didCreateAccount);
+		assertTrue("Didn't create account in the repository", didCreateAccount);
+	}
+	
+	@Test
+	public void testAddAccountBankTransactionException() {
+		try {
+			controller.addAccount(accountB);
+		} catch (BankTransactionException e) {
+			return;
+		}
+		
+		fail("Should throw BankTransactionException");
 	}
 	
 	@Test
 	public void testRemoveAccount() {
 		try {
-			controller.addAccount(account);
+			controller.removeAccount(ACCOUNT_NUMBER_A);
 		} catch (BankTransactionException e) {
 			fail(e.getMessage());
 		}
 		
+		assertTrue("Didn't remove account in the repository", didRemoveAccount);
+	}
+	
+	@Test
+	public void testRemoveAccountBankTransactionException() {
 		try {
-			controller.removeAccount(account.getNumber());
+			controller.removeAccount(INVALID_ACCOUNT_NUMBER);
 		} catch (BankTransactionException e) {
-			fail(e.getMessage());
+			return;
 		}
 		
-		assertTrue("Account wasn't removed of the repository", didRemoveAccount);
+		fail("Should throw BankTransactionException");
 	}
 	
 	@Test
 	public void testDoCredit() {
 		try {
-			controller.doCredit(account.getNumber(), 30);
+			controller.doCredit(ACCOUNT_NUMBER_A, 30);
 		} catch (BankTransactionException e) {
 			fail(e.getMessage());
 		}
 		
-		assertTrue("Account balance should be 80, is " + account.getBalance(), account.getBalance() == 80);
+		assertTrue("Account balance should be 80", accountA.getBalance() == 80);
+	}
+	
+	@Test
+	public void testDoCreditBankTransactionExceptionInvalidAccount() {
+		try {
+			controller.doCredit(INVALID_ACCOUNT_NUMBER, 30);
+		} catch (BankTransactionException e) {
+			return;
+		}
+		
+		fail("Should throw BankTransactionException");
+	}
+	
+	@Test
+	public void testDoCreditBankTransactionExceptionNegativeAmount() {
+		try {
+			controller.doCredit(ACCOUNT_NUMBER_A, -30);
+		} catch (BankTransactionException e) {
+			return;
+		}
+		
+		fail("Should throw BankTransactionException");
 	}
 	
 	@Test
 	public void testDoDebit() {
 		try {
-			controller.doDebit(account.getNumber(), 30);
+			controller.doDebit(ACCOUNT_NUMBER_A, 30);
 		} catch (BankTransactionException e) {
 			fail(e.getMessage());
 		}
 		
-		assertTrue("Account balance should be 20, is " + account.getBalance(), account.getBalance() == 20);
+		assertTrue("Account balance should be 20", accountA.getBalance() == 20);
+	}
+	
+	@Test
+	public void testDoDebitBankTransactionExceptionInvalidAccount() {
+		try {
+			controller.doDebit(INVALID_ACCOUNT_NUMBER, 30);
+		} catch (BankTransactionException e) {
+			return;
+		}
+		
+		fail("Should throw BankTransactionException");
+	}
+	
+	@Test
+	public void testDoDebitBankTransactionExceptionNegativeAmount() {
+		try {
+			controller.doDebit(ACCOUNT_NUMBER_A, -30);
+		} catch (BankTransactionException e) {
+			return;
+		}
+		
+		fail("Should throw BankTransactionException");
+	}
+	
+	@Test
+	public void testGetBalance() {
+		double balance = 0;
+		try {
+			balance = controller.getBalance(ACCOUNT_NUMBER_A);
+		} catch (BankTransactionException e) {
+			fail(e.getMessage());
+		}
+		
+		assertTrue("Balance should be 50", balance == 50);
+	}
+	
+	@Test
+	public void testGetBalanceBankTransactionException() {
+		try {
+			controller.getBalance(INVALID_ACCOUNT_NUMBER);
+		} catch (BankTransactionException e) {
+			return;
+		}
+		
+		fail("Should throw BankTransactionException");
+	}
+	
+	@Test
+	public void testDoTranfer() {
+		try {
+			controller.doTransfer(ACCOUNT_NUMBER_A, ACCOUNT_NUMBER_B, 30);
+		} catch (BankTransactionException e) {
+			fail(e.getMessage());
+		}
+		
+		assertTrue("Account A balance should be 20", accountA.getBalance() == 20);
+		assertTrue("Account B balance should be 30", accountB.getBalance() == 30);
+	}
+	
+	@Test
+	public void testDoTranferBankTransactionExceptionInvalidAccountA() {
+		try {
+			controller.doTransfer(INVALID_ACCOUNT_NUMBER, ACCOUNT_NUMBER_B, 30);
+		} catch (BankTransactionException e) {
+			assertTrue("Account B balance should be 0", accountB.getBalance() == 0);
+			return;
+		}
+		
+		fail("Should throw BankTransactionException");
+	}
+	
+	@Test
+	public void testDoTranferBankTransactionExceptionInvalidAccountB() {
+		try {
+			controller.doTransfer(ACCOUNT_NUMBER_A, INVALID_ACCOUNT_NUMBER, 30);
+		} catch (BankTransactionException e) {
+			assertTrue("Account A balance should be 50", accountA.getBalance() == 50);
+			return;
+		}
+		
+		fail("Should throw BankTransactionException");
+	}
+	
+	@Test
+	public void testDoTranferBankTransactionExceptionNegativeAmount() {
+		try {
+			controller.doTransfer(ACCOUNT_NUMBER_A, ACCOUNT_NUMBER_B, -30);
+		} catch (BankTransactionException e) {
+			assertTrue("Account A balance should be 50", accountA.getBalance() == 50);
+			assertTrue("Account B balance should be 0", accountB.getBalance() == 0);
+			return;
+		}
+		
+		fail("Should throw BankTransactionException");
+	}
+	
+	@Test
+	public void testDoTranferBankTransactionExceptionInsuficientFunds() {
+		try {
+			controller.doTransfer(ACCOUNT_NUMBER_A, ACCOUNT_NUMBER_B, 60);
+		} catch (BankTransactionException e) {
+			assertTrue("Account A balance should be 50", accountA.getBalance() == 50);
+			assertTrue("Account B balance should be 0", accountB.getBalance() == 0);
+			return;
+		}
+		
+		fail("Should throw BankTransactionException");
+	}
+	
+	@Test
+	public void testDoEarnInterest() {
+		try {
+			controller.doEarnInterest(SAVINGS_ACCOUNT_NUMBER);
+		} catch (BankTransactionException e) {
+			fail(e.getMessage());
+		}
+		
+		double expectedBalance = 50 + (50 * 0.001);
+		assertTrue("Savings account balance should be " + expectedBalance, savingsAccount.getBalance() == expectedBalance);
+	}
+	
+	@Test
+	public void testDoEarnInterestBankTransactionExceptionInvalidAccount() {
+		try {
+			controller.doEarnInterest(INVALID_ACCOUNT_NUMBER);
+		} catch (IncompatibleAccountException e) {
+			fail(e.getMessage());
+		} catch (BankTransactionException e) {
+			return;
+		}
+		
+		fail("Should throw BankTransactionException");
+	}
+	
+	@Test
+	public void testDoEarnInterestIncompatibleAccountException() {
+		try {
+			controller.doEarnInterest(ACCOUNT_NUMBER_A);
+		} catch (IncompatibleAccountException e) {
+			return;
+		} catch (BankTransactionException e) {
+			fail(e.getMessage());
+		}
+		
+		fail("Should throw IncompatibleAccountException");
+	}
+	
+	@Test
+	public void testDoEarnBonus() {
+		try {
+			controller.doEarnBonus(SPECIAL_ACCOUNT_NUMBER);
+		} catch (BankTransactionException e) {
+			fail(e.getMessage());
+		}
+		
+		double expectedBalance = 50 + (50 * 0.01);
+		assertTrue("Special Account balance should be " + expectedBalance, specialAccount.getBalance() == expectedBalance);
+	}
+	
+	@Test
+	public void testDoEarnBonusBankTransactionExceptionInvalidAccount() {
+		try {
+			controller.doEarnBonus(INVALID_ACCOUNT_NUMBER);
+		} catch (IncompatibleAccountException e) {
+			fail(e.getMessage());
+		} catch (BankTransactionException e) {
+			return;
+		}
+		
+		fail("Should throw BankTransactionException");
+	}
+	
+	@Test
+	public void testDoEarnBonusIncompatibleAccountException() {
+		try {
+			controller.doEarnBonus(ACCOUNT_NUMBER_A);
+		} catch (IncompatibleAccountException e) {
+			return;
+		} catch (BankTransactionException e) {
+			fail(e.getMessage());
+		}
+		
+		fail("Should throw IncompatibleAccountException");
 	}
 
 }
