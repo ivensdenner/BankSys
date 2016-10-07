bd929d930b5164046e74fa56e9959b2df1537d26
ivensdenner <ivensdenner@gmail.com>
2016-10-06 22:50:10 -0300

    Add testDebitNegativeAmount and testDebitInsuficientFunds to TaxAccountTest

--- test/banksys/account/TaxAccountTest.java
+++ test/banksys/account/TaxAccountTest.java
@@ -1,50 +1,61 @@
 package banksys.account;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import banksys.account.exception.InsufficientFundsException;
 import banksys.account.exception.NegativeAmountException;
 
 public class TaxAccountTest {
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
+	
+	private static final String ACCOUNT_NUMBER = "1234";
+	private TaxAccount account;
 
 	@Before
 	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
+		account = new TaxAccount(ACCOUNT_NUMBER);
+		account.credit(50);
 	}
 
 	@Test
 	public void testDebit() {
-		TaxAccount account = new TaxAccount("1234");
-		try {
-			account.credit(50);
-		} catch (NegativeAmountException e) {
-			fail(e.getMessage());
-		}
-		
 		try {
 			account.debit(30);
 		} catch (NegativeAmountException | InsufficientFundsException e) {
 			fail(e.getMessage());
 		}
 		
-		assertTrue("Balance should be " + (50 - (30 + (30 * 0.001))) + ", is " + account.getBalance(), account.getBalance() == 50 - (30 + (30 * 0.001)));
+		double expectedBalance = 50 - (30 + (30 * 0.001));
+		assertTrue("Balance should be " + expectedBalance, account.getBalance() == expectedBalance);
+	}
+	
+	@Test
+	public void testDebitNegativeAmount() {
+		try {
+			account.debit(-30);
+		} catch (InsufficientFundsException e) {
+			fail(e.getMessage());
+		} catch (NegativeAmountException e) {
+			return;
+		}
+		
+		fail("Should throw NegativeAmountException");
+	}
+	
+	@Test
+	public void testDebitInsufficientFundsException() {
+		try {
+			account.debit(80);
+		} catch (InsufficientFundsException e) {
+			return;
+		} catch (NegativeAmountException e) {
+			fail(e.getMessage());
+		}
+		
+		fail("Should throw InsufficientFundsException");
 	}
 
 }
